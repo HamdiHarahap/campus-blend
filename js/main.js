c007f068db163f859f32de0c9ff25d6c6c5d78f5
HamdiHarahap <hamdiharahap2005@gmail.com>
2024-04-16 22:08:53 +0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+feather.replace();
+
 const foodButton = document.getElementsByClassName('food')
 const drinkButton = document.getElementsByClassName('drink')
 const drink = document.querySelector('.collection-drink')
@@ -12,6 +14,28 @@ drinkButton[1].addEventListener('click', function() {
     food.style.display = 'none'
     drink.style.display = 'flex'
 })
+
+const menuIcon = document.querySelector('.menu-icon')
+const navMenu = document.querySelector('.nav-bar ul')
+const navClose = document.querySelector('.close-icon')
+
+menuIcon.addEventListener('click', function () {
+    navMenu.style.visibility = 'visible'
+    navMenu.style.height = 'auto'
+    menuIcon.style.visibility = 'hidden'
+    menuIcon.style.width = '0'
+    navClose.style.visibility = 'visible'
+    navClose.style.width = 'auto'
+})
+
+navClose.addEventListener('click', function () {
+    navMenu.style.visibility = 'hidden'
+    menuIcon.style.visibility = 'visible'
+    menuIcon.style.width = 'auto'
+    navClose.style.visibility = 'hidden'
+    navClose.style.width = '0'
+})
+
 
 const contents = document.querySelectorAll('.menu-box');
 
