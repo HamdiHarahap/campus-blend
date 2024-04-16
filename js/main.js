e86faafd7e87983a5347aeb52f9e0e0e6f9188ce
HamdiHarahap <hamdiharahap2005@gmail.com>
2024-04-16 09:14:32 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -17,6 +17,6 @@ const contents = document.querySelectorAll('.menu-box');
 
 contents.forEach(content => {
     content.addEventListener('mouseenter', function () {
-        this.classList.add('content-e');
+        this.classList.add('content-e')
     });
 });
